4dba446a4e85c5d10a0585b6344d5332e7b6bf0d
Kudrat10 <142271911+Kudrat10@users.noreply.github.com>
2024-08-25 14:21:09 +0530

    Delete microgrid-master/microgrid-master directory

--- microgrid-master/microgrid-master/backend/constants.js
+++
@@ -1,4 +0,0 @@
-export const config = {
-  secret: 'supratan' // TODO Put in process.env
-
-};
--- microgrid-master/microgrid-master/backend/services/auth/routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as controller from './controller.js';
-import User from '../../models/user.js'
-import mongoose from 'mongoose';
-import uri from '../../mondb.js'
-export const authRouter = express.Router();
-/** POST /api/auth */
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
-authRouter.route('/').post(controller.createMongo);
